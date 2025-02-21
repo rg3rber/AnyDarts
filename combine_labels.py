730697d9284edf81c220980cf86cce704571ae66
Reto Gerber <reto.gerber@lightray.io>
2025-02-21 13:33:55 +0100

    modify combine labels to only take in pkl files and add easy1 test set

--- combine_labels.py
+++ combine_labels.py
@@ -1,9 +1,12 @@
 import pandas as pd
 import os
 
+def is_pkl_file(file):
+    return file.endswith('.pkl')
+
 path = 'dataset/annotations/'
 # Get list of files in the directory
-label_files = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
+label_files = [f for f in os.listdir(path) if is_pkl_file(os.path.join(path, f))]
 
 labels = []
 for label_file in label_files:
